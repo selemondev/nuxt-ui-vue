c31ce4e61210cbc8da4b203e3581d6dc4d870796
Selemondev <selemondev@Selemondevs-MacBook-Pro.local>
2023-09-10 12:48:09 +0300

    chore: cleanup

--- packages/nuxt-ui-vue/src/App.vue
+++ packages/nuxt-ui-vue/src/App.vue
@@ -2,11 +2,5 @@
 </script>
 
 <template>
-  <div class="grid place-items-center min-h-screen w-full">
-    <UAvatarGroup size="sm" :max="2">
-      <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="benjamincanac" />
-      <UAvatar src="https://avatars.githubusercontent.com/u/904724?v=4" alt="Atinux" />
-      <UAvatar src="https://avatars.githubusercontent.com/u/7547335?v=4" alt="smarroufin" />
-    </UAvatarGroup>
-  </div>
+  <div />
 </template>
