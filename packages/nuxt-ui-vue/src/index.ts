223ebd2bfc1486880a474340bf5d89515b95c709
System Administrator <root@Selemondevs-MacBook-Pro.local>
2023-09-12 16:39:20 +0300

    feat: export composables

--- packages/nuxt-ui-vue/src/index.ts
+++ packages/nuxt-ui-vue/src/index.ts
@@ -2,6 +2,12 @@ export * from './components'
 
 export { default as createUI } from './plugin'
 
+export { defineShortcuts } from '@/composables/defineShortcuts'
+
+export { useCopyToClipboard } from '@/composables/useCopyToClipboard'
+
+export { useShortcuts } from '@/composables/useShortcuts'
+
 export { default as useToast } from '@/composables/useToast'
 
 export { NuxtUIVueComponentResolver } from '@/componentResolver'
